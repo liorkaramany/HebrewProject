fa24c40e79795cf17fd1ad4fff576351f3dd107e
Lior Karamany <lior.karamany@gmail.com>
2018-03-12 20:52:47 +0200

    Added more verbs (מרובעים)

--- app/src/main/java/com/example/user/hebrewproject/Test1.java
+++ app/src/main/java/com/example/user/hebrewproject/Test1.java
@@ -8,9 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -28,6 +30,9 @@ public class Test1 extends AppCompatActivity {
     ArrayAdapter<CharSequence> atime;
     ArrayAdapter<CharSequence> agizra;
 
+    TextView a0, a1, a2, a3, a4;
+    Button btn;
+
     Random rnd=new Random();
 
     int n;
@@ -43,6 +48,14 @@ public class Test1 extends AppCompatActivity {
         time = (Spinner) findViewById(R.id.time);
         gizra = (Spinner) findViewById(R.id.gizra);
         root=(EditText) findViewById(R.id.root);
+
+        a0=(TextView) findViewById(R.id.a0);
+        a1=(TextView) findViewById(R.id.a1);
+        a2=(TextView) findViewById(R.id.a2);
+        a3=(TextView) findViewById(R.id.a3);
+        a4=(TextView) findViewById(R.id.a4);
+
+        btn=(Button) findViewById(R.id.btn);
 
         Intent gt=getIntent();
         dif=gt.getIntExtra("dif", 1);
@@ -74,9 +87,9 @@ public class Test1 extends AppCompatActivity {
         {
             switch (topic)
             {
-                case 1: n1=1; n2=20; break;
-                case 2: n1=21; n2=40; break;
-                case 3: n1=41; n2=60; break;
+                case 1: n1=1; n2=40; break;
+                case 2: n1=41; n2=60; break;
+                case 3: n1=61; n2=80; break;
             }
         }
 
@@ -90,50 +103,74 @@ public class Test1 extends AppCompatActivity {
     }
 
     public void next(View view) {
-        Resources res = getResources();
-        int resID = res.getIdentifier("v"+n, "array", getPackageName());
-        String[] verb = res.getStringArray(resID);
-
-        if (root.getText().toString().equals(""))
-            Toast.makeText(this, "לא הכנסת שורש", Toast.LENGTH_SHORT).show();
-        else
+        if (!btn.getText().toString().equals("הבא"))
         {
-            //Check the answers.
-            if (verb[0].equals(root.getText().toString()))
-                points+=0.2;
-            if (verb[1].equals(build.getSelectedItem()))
-                points+=0.2;
-            if (verb[2].equals(body.getSelectedItem()))
-                points+=0.2;
-            if (verb[3].equals(time.getSelectedItem()))
-                points+=0.2;
-            if (verb[4].equals(gizra.getSelectedItem()))
-                points+=0.2;
+           Resources res = getResources();                                                                
+           int resID = res.getIdentifier("v" + n, "array", getPackageName());                             
+           String[] verb = res.getStringArray(resID);
 
-            Intent t=null;
-            if (round==10)
-                t=new Intent(this, Results.class);
-            else if (topic>=4)
-                switch (rnd.nextInt(3)) {
-                    case 0: t=new Intent(this, Test1.class);
-                        break;
-                    case 1: t=new Intent(this, Test2.class);
-                        break;
-                    case 2: t=new Intent(this, Test3.class);
-                        break;
-                }
-            else
-                t=new Intent(this, Test1.class);
+           if (root.getText().toString().equals(""))
+               Toast.makeText(this, "לא הכנסת שורש", Toast.LENGTH_SHORT).show();
+           else
+           {
+               a0.setText(""+verb[0]);
+               a1.setText(""+verb[1]);
+               a2.setText(""+verb[2]);
+               a3.setText(""+verb[3]);
+               a4.setText(""+verb[4]);
 
-            round++;
+               //Check the answers.
+               if (verb[0].equals(root.getText().toString()))
+                   points += 0.2;
+               if (verb[1].equals(build.getSelectedItem()))
+                   points += 0.2;
+               if (verb[2].equals(body.getSelectedItem()))
+                   points += 0.2;
+               if (verb[3].equals(time.getSelectedItem()))
+                   points += 0.2;
+               if (verb[4].equals(gizra.getSelectedItem()))
+                   points += 0.2;
 
-            t.putExtra("points", points);
-            t.putExtra("round", round);
-            t.putExtra("dif", dif);
-            t.putExtra("topic", topic);
-            t.putExtra("subject", subject);
+               btn.setText("הבא");
+           }
+        }
+        else {
+            Resources res = getResources();
+            int resID = res.getIdentifier("v" + n, "array", getPackageName());
+            String[] verb = res.getStringArray(resID);
 
-            startActivity(t);
+            if (root.getText().toString().equals(""))
+                Toast.makeText(this, "לא הכנסת שורש", Toast.LENGTH_SHORT).show();
+            else {
+
+                Intent t = null;
+                if (round == 20)
+                    t = new Intent(this, Results.class);
+                else if (topic >= 4)
+                    switch (rnd.nextInt(3)) {
+                        case 0:
+                            t = new Intent(this, Test1.class);
+                            break;
+                        case 1:
+                            t = new Intent(this, Test2.class);
+                            break;
+                        case 2:
+                            t = new Intent(this, Test3.class);
+                            break;
+                    }
+                else
+                    t = new Intent(this, Test1.class);
+
+                round++;
+
+                t.putExtra("points", points);
+                t.putExtra("round", round);
+                t.putExtra("dif", dif);
+                t.putExtra("topic", topic);
+                t.putExtra("subject", subject);
+
+                startActivity(t);
+            }
         }
     }
 }
