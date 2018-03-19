fbe3e0072f53bfba1b8a4a7b9ac440b6bf55cded
Lior Karamany <lior.karamany@gmail.com>
2018-03-19 19:06:14 +0200

    Designed the nouns screen (Yet it still doesn't do anything) :P

--- app/src/main/java/com/example/user/hebrewproject/Test2.java
+++ app/src/main/java/com/example/user/hebrewproject/Test2.java
@@ -2,6 +2,7 @@ package com.example.user.hebrewproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Test2 extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Test2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test2);
+    }
+
+    public void next(View view) {
     }
 }
